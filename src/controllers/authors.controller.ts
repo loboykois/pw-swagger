97d511caee37cc1fce08fc3418b3f23b64568507
Loboyko Ilya <loboykois@gmail.com>
2025-05-28 00:22:29 +0300

    test: add custom status codes

--- src/controllers/authors.controller.ts
+++ src/controllers/authors.controller.ts
@@ -1,7 +1,8 @@
-import { expect } from '@playwright/test';
 import { Config } from '../../config';
 import { BaseController } from './base.controller';
 import { components } from '../../.temp/types';
+import { API_STATUSES } from '../../utils/api-statuses';
+import { expect } from '@playwright/test';
 
 export type Author = components['schemas']['Author'];
 export type Authors = components['schemas']['Author'][];
@@ -12,7 +13,7 @@ export class AuthorsController extends BaseController {
   async getAuthors() {
     const response = await this.request().url(this.url).method('GET').send<Authors>();
 
-    expect(response.statusCode).toBe(200);
+    expect(response.statusCode).toBe(API_STATUSES.SUCCESSFUL_200_STATUS);
 
     return response.body;
   }
@@ -20,7 +21,7 @@ export class AuthorsController extends BaseController {
   async getAuthorById(id: number) {
     const response = await this.request().url(`${this.url}/${id}`).method('GET').send<Author>();
 
-    expect(response.statusCode).toBe(200);
+    expect(response.statusCode).toBe(API_STATUSES.SUCCESSFUL_200_STATUS);
 
     return response.body;
   }
