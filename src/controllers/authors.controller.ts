430ad2f81f7b79c12233b7389e2f97fab7257f8b
Loboyko Ilya <loboykois@gmail.com>
2025-05-27 23:57:01 +0300

    test: add return types in AuthorController

--- src/controllers/authors.controller.ts
+++ src/controllers/authors.controller.ts
@@ -1,10 +1,24 @@
 import { expect } from '@playwright/test';
 import { Config } from '../../config';
 import { BaseController } from './base.controller';
+import { components } from '../../.temp/types';
+
+export type Author = components['schemas']['Author'];
+export type Authors = components['schemas']['Author'][];
 
 export class AuthorsController extends BaseController {
+  private readonly url = `${Config.BASE_URL}/api/v1/Authors`;
+
   async getAuthors() {
-    const response = await this.request().url(`${Config.BASE_URL}/api/v1/Authors`).send();
+    const response = await this.request().url(this.url).method('GET').send<Authors>();
+
+    expect(response.statusCode).toBe(200);
+
+    return response.body;
+  }
+
+  async getAuthorById(id: number) {
+    const response = await this.request().url(`${this.url}/${id}`).method('GET').send<Author>();
 
     expect(response.statusCode).toBe(200);
 
